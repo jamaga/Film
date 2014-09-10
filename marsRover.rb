effb1374da8ba37d6eadf964d9eafde8f0509439
aga <hipo.beaver@gmail.com>
2014-09-10 15:25:57 +0200

    check if can move and if off the planet

--- marsRover.rb
+++ marsRover.rb
@@ -18,6 +18,14 @@ class Planet
     @grid[x][y] = ''
   end
 
+  def check_if_can_move(wsp_x, wsp_y) #czy pod tymi wspolrzednymi znajduje sie rover musi sprawdzac
+    if @grid[wsp_x][wsp_y] == 'r' #jesli znajduje sie rower to false bo nie moze sie ruszy cjak noe to true
+      return false
+    else
+      return true
+    end
+  end
+
 end
 
 
@@ -30,6 +38,7 @@ class Rover
     @planet.save_rover_position(x, y)
 
   end
+
 
   def move
     @planet.remove_rover_position(@x, @y)
@@ -85,13 +94,37 @@ class Rover
      @direction ='w'
     end
   end
+
+
+  def off_the_planet?
+    raise "The Rover Fell Off the Planet!" if @y < 0 || @x < 0
+  end
+
+ 
+
+  def handle_obstacle next_position
+    if @planet.obstacle? next_position
+      @planet.save_rover_position
+    else
+    next_position
+  end
 end
+
+end
+
+
 
 kata_planet = Planet.new
 
-new_rover = Rover.new(3, 2, 's', kata_planet)
+new_rover = Rover.new(1, 2, 's', kata_planet)
+
+crash_rover = Rover.new(1, 3, 'w', kata_planet)
 
 
+p crash_rover
+
+
+p kata_planet.check_if_can_move(2, 3) #wywoluje na planecie bo metoda jest zdef na kalsie planet
 
 
 
